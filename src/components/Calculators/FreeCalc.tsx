0ae9d895cebc991aeb457dc26c7d687257a5a3ca
danielle <tangeringe0.0tail@gmail.com>
2025-06-30 18:00:21 +0900

    style :

--- src/components/Calculators/FreeCalc.tsx
+++ src/components/Calculators/FreeCalc.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type FreeCalcProps = {
   onDataChange: (data: {amount: number; isValid: boolean}) => void;
@@ -8,24 +8,38 @@ export default function FreeCalc({onDataChange}:FreeCalcProps) {
   const [amount, setAmount] = useState<string>('');
   const [isValid, setIsValid] = useState<boolean>(false);
 
-  const handleAmountChange = () =>{
-    
+  // 유효성 검사
+  const validateAmount = (value:string): boolean => {
+    return !isNaN(parseInt(value)) && parseInt(value) > 0;
   }
 
+  const handleAmountChange = (e) =>{
+    setAmount(e.target.value);
+    const valid = validateAmount(e.target.value);
+    setIsValid(valid);
+  }
+
+  useEffect(()=>{
+    const num = parseInt(amount) || 0;
+    onDataChange({amount: num, isValid});
+  },[amount, isValid, onDataChange]);
+
   return (
-    <div className='flex'>
-      <label className=''>
-        지급액 <span className="text-red-50">*</span>
-      </label>
-      <div className="">
-        <input 
-          type="text"
-          className=''
-          value={amount}
-          onChange={handleAmountChange}
-          placeholder='0'
-        />
-        <span className="">원</span>
+    <div className='text-left'>
+      <div className="flex w-full">
+        <label className=''>
+          지급액 <span className="text-red-50">*</span>
+        </label>
+        <div className="flex grow">
+          <input 
+            type="text"
+            className='grow text-right'
+            value={amount}
+            onChange={handleAmountChange}
+            placeholder='0'
+          />
+          <span className="">원</span>
+        </div>
       </div>
       {amount && !isValid && (
         <p className="">
