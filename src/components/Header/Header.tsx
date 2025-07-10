d327bdfdc703130e9c3122577f738ddaef2b70ca
danielle <tangeringe0.0tail@gmail.com>
2025-07-10 12:22:12 +0900

    design: 파비콘, 로고
    파비콘과 로고 이미지 삽입, svg global.d.ts 설정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import logo from '../../assets/images/shrimply_logo_color.svg';
 
 export default function Header () {
   return (
-    <header></header>
+    <header className='mb-10'>
+      <div className='' style={{"width":'150px'}}>
+        <img src={logo} alt="" />
+      </div>
+    </header>
   )
 }
