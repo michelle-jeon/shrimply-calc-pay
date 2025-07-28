4600f6e5f51c8ac78819966d895b42deff13e38d
danielle <tangerine0.0tail@gmail.com>
2025-07-28 17:24:00 +0900

    test: 브랜치 분리 테스트

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const FooterSection = styled.div`
   width:100%;
-  margin-top:225px;
+  margin-top:125px;
   background-color:#fff;
   padding:30px 0;
   color:#8B95A1;
