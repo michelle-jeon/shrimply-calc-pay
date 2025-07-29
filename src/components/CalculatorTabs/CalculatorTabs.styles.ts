c78b888c832f536be6f4bce7fef4cf24eaefbb3d
danielle <tangerine0.0tail@gmail.com>
2025-07-29 16:54:06 +0900

    design: tabs
    모바일 사이즈 디자인

--- src/components/CalculatorTabs/CalculatorTabs.styles.ts
+++ src/components/CalculatorTabs/CalculatorTabs.styles.ts
@@ -3,19 +3,13 @@ import styled from 'styled-components';
 
 export const TabsContainer = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap:8px;
   background-color: white;
-  border-radius: 0.5rem;
+  border-radius: 9999px;
   padding: 0.5rem;
-  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    border-radius: 9999px;
-    padding: 0.5rem;
-  }
+  box-shadow: 0px 2px 30px 0px rgba(0, 27, 55, 0.10);
 `;
 
 export const TabButton = styled.button<{
@@ -24,12 +18,13 @@ export const TabButton = styled.button<{
   display: flex;
   align-items: center;
   width: 100%;
-  padding: 0.5rem 1rem;
+  padding: 7px 27px;
   border-radius: 9999px;
   transition: all 0.2s ease-in-out;
-  background-color: ${({ $isActive }) => ($isActive ? '#E5E7EB' : 'transparent')};
+  background-color: ${({ $isActive }) => ($isActive ? 'rgba(2, 32, 71, 0.05)' : 'transparent')};
   font-weight: ${({ $isActive }) => ($isActive ? 'bold' : 'normal')};
   color: ${({ $isActive }) => ($isActive ? '#111827' : '#4B5563')};
+  word-break: keep-all;
 
   &:hover {
     background-color: ${({ $isActive }) => ($isActive ? '#E5E7EB' : '#F3F4F6')};
@@ -55,6 +50,10 @@ export const Separator = styled.span`
 `;
 
 export const TabInfo = styled.span`
+  display: none;
   font-size: 0.875rem;
   color: #6B7280;
+  @media (min-width: 768px) {
+    display: inline;
+  }
 `;
